b1f4cc8c2c45c863fa791e7d02b1644a2c4e3098
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-08-17 03:23:31 +0300

    Adds the Quotes to the quotes component.ts from our main component app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quotes';
-  quotes =[
-    {id:1,name:"You have the chance to change the world."},
-    {id:2,name:"Life is what you make it."},
-    {id:3,name:"If your ship does not come in,swim out to it."},
-    {id:4,name:"Don’t Let Yesterday Take Up Too Much Of Today"},
-    {id:5,name:"You Learn More From Failure Than From Success."},
-    {id:6,name:"Whether You Think You Can Or Think You Can’t, You’re Right"}
-
-  ]
+  title = 'GALLORE DE QUOTES';
+ 
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quotes';
+
+
 
 @Component({
   selector: 'app-quotes',
@@ -6,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  
+   quotes:Quote = [
+    {id:1,name:"You have the chance to change the world."},
+    {id:2,name:"Life is what you make it."},
+    {id:3,name:"If your ship does not come in,swim out to it."},
+    {id:4,name:"Don’t Let Yesterday Take Up Too Much Of Today"},
+    {id:5,name:"You Learn More From Failure Than From Success."},
+    {id:6,name:"Whether You Think You Can Or Think You Can’t, You’re Right"},
 
+  ];
+  
   constructor() { }
 
   ngOnInit() {

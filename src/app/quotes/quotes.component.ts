f4f6e4f147cbad5f62925289013aa198f8aae1b5
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-08-17 03:32:28 +0300

    Adds the posting details component.

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,12 +11,12 @@ import { Quote } from '../quotes';
 export class QuotesComponent implements OnInit {
   
    quotes:Quote = [
-    {id:1,name:"You have the chance to change the world."},
-    {id:2,name:"Life is what you make it."},
-    {id:3,name:"If your ship does not come in,swim out to it."},
-    {id:4,name:"Don’t Let Yesterday Take Up Too Much Of Today"},
-    {id:5,name:"You Learn More From Failure Than From Success."},
-    {id:6,name:"Whether You Think You Can Or Think You Can’t, You’re Right"},
+    {id:1,name:"You have the chance to change the world.",owner:"By Ndundiro Kamau"},
+    {id:2,name:"Life is what you make it.",owner:"By Ndundiro Kamau"},
+    {id:3,name:"If your ship does not come in,swim out to it.",owner:"By Ndundiro Kamau"},
+    {id:4,name:"Don’t Let Yesterday Take Up Too Much Of Today",owner:"By Ndundiro Kamau"},
+    {id:5,name:"You Learn More From Failure Than From Success.",owner:"By Ndundiro Kamau"},
+    {id:6,name:"Whether You Think You Can Or Think You Can’t, You’re Right",owner:"By Ndundiro Kamau"},
 
   ];
   
